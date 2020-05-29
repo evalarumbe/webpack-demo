4b4e38024e71668c0cfe8df5cff1d419e16b7c88
Eva Larumbe <evalarumbe@gmail.com>
2020-05-30 00:29:26 +0200

    Remove code and assets not needed in next tutorial

--- src/index.js
+++ src/index.js
@@ -1,29 +1,11 @@
 import _ from 'lodash';
-import './style.css';
-import NyanCat from './nyan.gif';
-import CsvData from './data/Toggl_summary_report_2020-05-25_2020-05-31.csv';
-import XmlData from './data/data.xml';
 
 function component() {
   const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
 
   return element;
 }
 
-function image() {
-  const image = document.createElement('img');
-
-  image.setAttribute('src', NyanCat);
-  image.setAttribute('alt', 'Nyan cat hurtling gleefully through space');
-
-  return image;
-}
-
 document.body.appendChild(component());
-document.body.appendChild(image());
-
-console.log(CsvData);
-console.log(XmlData);
