5b40dfa09cbd06e6912f4941de7ccb4d98b4c4bd
Eva Larumbe <evalarumbe@gmail.com>
2020-05-29 23:37:13 +0200

    Use file-loader to reference an image asset in CSS with url()

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,12 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /.(png|svg|jpe?g|gif)$/,
+        use: [
+          'file-loader',
+        ],
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
