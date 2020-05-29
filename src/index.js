c04b213c0b2726fd5d58583a5cb2472bd2c5342a
Eva Larumbe <evalarumbe@gmail.com>
2020-05-30 00:20:14 +0200

    Use loaders to log csv and xml data to the console

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import _ from 'lodash';
 import './style.css';
 import NyanCat from './nyan.gif';
+import CsvData from './data/Toggl_summary_report_2020-05-25_2020-05-31.csv';
+import XmlData from './data/data.xml';
 
 function component() {
   const element = document.createElement('div');
@@ -22,3 +24,6 @@ function image() {
 
 document.body.appendChild(component());
 document.body.appendChild(image());
+
+console.log(CsvData);
+console.log(XmlData);
