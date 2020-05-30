3ae35c20291afddb91cf5db58a33817eb1bdcab9
Eva Larumbe <evalarumbe@gmail.com>
2020-05-31 00:11:04 +0200

    Fix: escape regex properly

--- webpack.config.js
+++ webpack.config.js
@@ -16,13 +16,13 @@ module.exports = {
         ]
       },
       {
-        test: /.(png|svg|jpe?g|gif)$/,
+        test: /\.(png|svg|jpe?g|gif)$/,
         use: [
           'file-loader',
         ],
       },
       {
-        test: /.(woff|woff2|eot|ttf|otf)$/,
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
         use: [
           'file-loader',
         ],
