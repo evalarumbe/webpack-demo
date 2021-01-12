494f1904b95cb7a40be2ed80ac1fb39453e87617
Eva Larumbe <evalarumbe@gmail.com>
2021-01-13 00:39:22 +0100

    Install Babel to support Promises (used by dynamic imports)

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,4 +4,13 @@ const common = require('./webpack.common.js');
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  }
 });
