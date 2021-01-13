84c601fc159ed06f44cddb23f57927298de7feeb
Eva Larumbe <evalarumbe@gmail.com>
2021-01-13 09:33:37 +0100

    Remove code not related to this exercise (printMe and HMR)

--- src/index.js
+++ src/index.js
@@ -1,38 +1,13 @@
 // external
-import _ from 'lodash';
-
-// local
-import printMe from './print.js';
+import _ from 'lodash'; // we're about to do this dynamically instead
 
 // styles
 import './styles.css';
 
 function component() {
   const element = document.createElement('div');
-  const btn = document.createElement('button');
-
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  btn.innerHTML = 'Click me and check the console!';
-  btn.addEventListener('click', printMe, false);
-
-  element.appendChild(btn);
-
   return element;
 }
 
-let elementToReload = component(); // original printMe
-document.body.appendChild(elementToReload);
-
-// Hot Module Reloader
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    document.body.removeChild(elementToReload);
-    elementToReload = component(); // re-render the component to use the updated printMe
-    document.body.appendChild(elementToReload);
-  });
-}
-
-// If HMR is working, this should only run once when we start webpack-dev-server
-window.addEventListener('load', function() { console.log(`LOADED! ${new Date()}`) });
+document.body.appendChild(component());
