3c5f253b31334e441e4ded1379122ada681c3129
Eva Larumbe <evalarumbe@gmail.com>
2021-01-13 14:29:41 +0100

    Use dynamic imports to implement lazy loading

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
+import _ from 'lodash';
+
 // styles
 import './styles.css';
 
-async function getComponent() {
-  const{ default: _ } = await import(/* webpackChunkName: "lodash" */ 'lodash'); // TODO: this destructuring syntax means what?
+function component() {
   const element = document.createElement('div');
+  
+  const button = document.createElement('button');
+  const br = document.createElement('br');
+  button.innerHTML = 'Click me and look at the console!';
+
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.appendChild(br);
+  element.appendChild(button);
+  
+  // Note that because a network request is involved, some indication
+  // of loading would need to be shown in a production-level site/app.
+  button.addEventListener('click', () => import(/* webbpackChunkName: "print" */ './print').then(module => {
+    const print = module.default;
+    print();
+  }).catch(error => "An error ocurred while appending the component"));
+
   return element;
 }
 
-getComponent().then(component => {
-  document.body.appendChild(component);
-}).catch(error => "An error ocurred while appending the component");
+document.body.appendChild(component());
